a703f9cd0942ec6a4fedf4718029b63d356046a3
THUC HUYEN <thuctonhuyen@gmail.com>
2019-03-10 14:28:10 -0700

    cleanup code: remove unsused code/ fix indentation

--- sw.js
+++ sw.js
@@ -43,11 +43,9 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function (event) {
-  if (event && event.request) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-      return response
-        ? response
-        : fetch(event.request);
-    }))
-  }
+  event.respondWith(caches.match(event.request).then(function (response) {
+    return response
+      ? response
+      : fetch(event.request);
+  }))
 })
